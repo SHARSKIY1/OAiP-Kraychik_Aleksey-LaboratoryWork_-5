7727f88e5b7b295c8d917c9eabc67d707aabf17e
Алексей <alexeikraichik@yandex.ru>
2026-01-22 14:45:32 +0300

    Добавьте файлы проекта.

---
+++ Laboratory5_№1/Equation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory5__1
+{
+    class Equation
+    {
+        public double first {  get; set; }
+        public double second { get; set; }
+        public double answer { get; set; }
+
+        public void Read()
+        {
+            Console.WriteLine("Введите первое число A: ");
+            string usernumber1 = Console.ReadLine();
+            first = double.Parse(usernumber1);
+            Console.WriteLine("Введите второе число B: ");
+            string usernumber2 = Console.ReadLine();
+            second = double.Parse(usernumber2);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(answer);
+        }
+
+        public void Function()
+        {
+            double x = 1;
+            answer = Math.Sqrt(first * x + second); 
+        }
+    }
+}
---
+++ Laboratory5_№1/Laboratory5_№1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory5__1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Я программа которая показывает лежит ли число введённое вами ");
+            Equation equation  = new Equation();
+            equation.Read();
+            equation.Function();
+            equation.Display();
+        }
+    }
+}
